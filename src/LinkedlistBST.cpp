4ef69fd9ae5be44744c9914f24cdb3460a78a193
aayusha <aayushaaaa.i@gmail.com>
2024-05-16 10:19:31 +0545

    member functions

---
+++ src/LinkedlistBST.cpp
@@ -0,0 +1,19 @@
+#include "../include/LinkedlistBST.h"
+#include <iostream>
+
+bool LinkedlistBST::isEmpty()
+    {
+
+    }
+void LinkedlistBST::add(int data)
+     {
+
+     }
+bool LinkedlistBST::remove(int key)
+     {
+
+     }
+bool LinkedlistBST::search(int targetkey)
+     {
+
+     }
