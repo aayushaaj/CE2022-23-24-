ca4c4404c30f645b668493e587ecd937d0210f31
aayushaaaa <aayushaaa.i@gmail.com>
2024-06-02 07:52:29 +0545

    class members updated

--- include/node.h
+++ include/node.h
@@ -1,8 +1,9 @@
 class node
 {
-    public:
-        int info;
-        int *root;
-        int *left;
-        int *right;
+public:
+    node(int key) : key(key), left(nullptr), right(nullptr), value(0) {}
+    int key;
+    node *left;
+    node *right;
+    int value;
 };
