07d7f28ea33c440673244c983a79bfbb2af47abc
aayushaaaa <aayushaaaa.i@gmail.com>
2024-06-02 20:01:01 +0545

    member visibility fixed

--- include/node.h
+++ include/node.h
@@ -1,9 +1,9 @@
 class node
 {
-public:
-    node(int key) : key(key), left(nullptr), right(nullptr), value(0) {}
     int key;
     node *left;
     node *right;
-    int value;
+
+public:
+    node(int key) : key(key), left(nullptr), right(nullptr) {}
 };
