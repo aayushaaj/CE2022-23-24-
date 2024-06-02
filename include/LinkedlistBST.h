b2e2b70f5cf807065699e27bc31108a6197b071b
aayushaaaa <aayushaaaa.i@gmail.com>
2024-06-02 20:00:26 +0545

    class member functions and constructor added

--- include/LinkedlistBST.h
+++ include/LinkedlistBST.h
@@ -1,10 +1,17 @@
+#pragma once
 #include "BST.h"
 
-class LinkedlistBST:public BST{
+class LinkedlistBST : public BST
+{
 
-    public:
-     bool isEmpty();
-     void addBST(int data);
-     bool removeBST(int keyToDeelete);
-     bool searchBST(int targetkey);
+public:
+    LinkedlistBST();
+    bool isEmpty();
+    void addBST(int data);
+    bool removeBST(int keyToDeelete);
+    bool searchBST(int targetkey);
+    void addNode(node *newNode, int data);
+    bool searchNode(node *current, int targetkey) const;
+    node *removeNode(node *current, int keyToDelete, bool isRemoved);
+    node *findMin(node *current) const;
 };
